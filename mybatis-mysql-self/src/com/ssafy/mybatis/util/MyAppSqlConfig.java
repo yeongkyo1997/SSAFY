75a12c05e05851951f7e7d7f577c83caedbdbd2e
yeongkyo1997 <yeongkyo1997@gmail.com>
2023-04-06 17:56:15 +0900

    commit

--- mybatis-mysql-self/src/com/ssafy/mybatis/util/MyAppSqlConfig.java
+++ mybatis-mysql-self/src/com/ssafy/mybatis/util/MyAppSqlConfig.java
@@ -1,24 +1,25 @@
 package com.ssafy.mybatis.util;
 
-import java.io.Reader;
-
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
-public class MyAppSqlConfig {
-	private static SqlSessionFactory sqlFactory;
-	static {
-		try {
-			String resource = "mybatis/sqlMapConfig.xml";
-			Reader reader = Resources.getResourceAsReader(resource);
-			sqlFactory = new SqlSessionFactoryBuilder().build(reader);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+import java.io.Reader;
 
-	public static SqlSessionFactory getSqlSessionFactory() {
-		return sqlFactory;
-	}
+public class MyAppSqlConfig {
+    private static SqlSessionFactory sqlFactory;
+
+    static {
+        try {
+            String resource = "mybatis/sqlMapConfig.xml";
+            Reader reader = Resources.getResourceAsReader(resource);
+            sqlFactory = new SqlSessionFactoryBuilder().build(reader);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static SqlSessionFactory getSqlSessionFactory() {
+        return sqlFactory;
+    }
 }
