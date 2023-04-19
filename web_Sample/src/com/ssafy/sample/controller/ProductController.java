aedf18cf0c672c60f8351c773282abc02e879459
yeongkyo1997 <yeongkyo1997@gmail.com>
2023-04-19 18:20:24 +0900

    commit

--- web_Sample/src/com/ssafy/sample/controller/ProductController.java
+++ web_Sample/src/com/ssafy/sample/controller/ProductController.java
@@ -1,58 +1,20 @@
 package com.ssafy.sample.controller;
 
-import java.io.IOException;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
-import com.ssafy.sample.dto.ProductDto;
-import com.ssafy.sample.model.service.ProductService;
-import com.ssafy.sample.model.service.ProductServiceImpl;
-
-@WebServlet("/product")
+@WebServlet
 public class ProductController extends HttpServlet {
-	private ProductService productService;
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
 
-	@Override
-	public void init() throws ServletException {
-		productService = ProductServiceImpl.getInstance();
-	}
-
-	@Override
-	protected void doPost(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-		request.setCharacterEncoding("utf-8");
-		String action = request.getParameter("action");
-
-		if (action == null)
-			action = "list";
-
-		switch (action) {
-		case "regist":
-			response.sendRedirect(request.getContextPath()+);
-			break;
-		}
-	}
-
-	@Override
-	protected void doGet(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-	}
-
-	private String regist(HttpServletRequest request, HttpServletResponse response) {
-		ProductDto productDto = new ProductDto();
-		productDto.setCode(request.getParameter("code"));
-		productDto.setModel(request.getParameter("model"));
-		productDto.setPrice(Integer.parseInt(request.getParameter("price")));
-		try {
-			productService.regist(productDto);
-		} catch (Exception e) {
-			e.printStackTrace();
-			return "/error/error.jsp";
-		}
-		return "/product?action=list";
-	}
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    }
 }
